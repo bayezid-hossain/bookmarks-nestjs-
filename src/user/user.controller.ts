4f52716c7880c76ff77799f59226b45ebd70fc19
Bayezid <arkantos.tas@gmail.com>
2025-02-03 12:45:36 +0600

    jwt token guard for api users/me api user info decorator

---
+++ src/user/user.controller.ts
@@ -0,0 +1,24 @@
+import { Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { Request } from 'express';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+@UseGuards(JwtGuard)
+export class UserController {
+    @Get('me')
+    getMe(@GetUser() user: User) {
+        //@GetUser('id') userId: number 
+        //@GetUser('email') email: string,
+        // console.log({
+        //     user: Req.user
+        // })
+
+        return user
+    }
+
+    @Patch()
+    editUser() { }
+}
